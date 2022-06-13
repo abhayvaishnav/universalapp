cf1b20e6c57bff5b0193bc247875fc7921ede718
Abhay Vaishnav <abhayvaishnav551@gmail.com>
2022-06-13 10:46:25 +0530

    Basic Notes App With Firebase in flutter

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:universalapp/style/app_style.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Universal Notes'),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+    );
+  }
+}
